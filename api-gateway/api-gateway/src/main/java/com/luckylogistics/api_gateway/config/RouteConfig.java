b77478810155768cea72cb06bde0477299e7ba9c
yeon-22k <pink5465y@gmail.com>
2025-11-05 00:52:27 +0900

    feat(gateway): setting

---
+++ api-gateway/api-gateway/src/main/java/com/luckylogistics/api_gateway/config/RouteConfig.java
@@ -0,0 +1,10 @@
+package com.luckylogistics.api_gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RouteConfig {
+
+}
