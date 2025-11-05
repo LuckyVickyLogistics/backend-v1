5215e2e09aaa478657c21655ad5caf1bb8e85947
yeon-22k <pink5465y@gmail.com>
2025-11-05 14:29:23 +0900

    chore(company): 엔드포인트 복수형으로 수정

--- company/company/src/main/java/com/luckylogistics/company/presentation/CompanyController.java
+++ company/company/src/main/java/com/luckylogistics/company/presentation/CompanyController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/company")
+@RequestMapping("/api/v1/companies")
 public class CompanyController {
     //todo: 인가 처리
 
