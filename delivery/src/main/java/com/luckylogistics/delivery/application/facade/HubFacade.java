3980f0d2b86a280fe616f51cf841c4d1e16b6614
김부경 <sa01023@naver.com>
2025-11-07 00:34:55 +0900

    feat(delivery): HubFacade에 허브 관리자 Hub ID 조회 메서드 추가

--- delivery/src/main/java/com/luckylogistics/delivery/application/facade/HubFacade.java
+++ delivery/src/main/java/com/luckylogistics/delivery/application/facade/HubFacade.java
@@ -23,7 +23,23 @@ public class HubFacade {
     public void validateHubExists(UUID hubId) {
         // TODO: Hub Service 연동
         // 허브 존재 하는지
+        // Hub hub = hubClient.getHub(hubId);
 
         log.warn("[TODO] Hub Service 연동 필요 - hubId 검증 생략: {}", hubId);
+    }
+
+    /**
+     * 요청 사용자의 Hub ID 조회
+     * HUB_MANAGER의 담당 허브 확인 (권한)
+     */
+    public UUID getUserHubId(Long userId) {
+        // TODO: Hub Service 연동
+        // HubManagerResponse manager = hubClient.getHubManager(userId);
+        // return manager.hubId();
+
+        // FIXME: 임시 하드코딩
+        UUID tempHubId = UUID.fromString("00000000-0000-0000-0000-000000000001");
+        log.warn("[TODO] Hub Service 연동 필요 - hubId 하드코딩: {}", tempHubId);
+        return tempHubId;
     }
 }
