38d074937a7521dc26266b37dc28290b331c3581
김부경 <sa01023@naver.com>
2025-11-07 00:30:49 +0900

    feat(delivery): 사용자 권한 Enum(UserRole) 추가

---
+++ delivery/src/main/java/com/luckylogistics/delivery/common/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.luckylogistics.delivery.common.enums;
+
+public enum UserRole {
+    MASTER_ADMIN , // 마스터 관리자
+    HUB_MANAGER, // 허브 관리자
+    DELIVERY_MANAGER, // 배송 담당자
+    COMPANY_MANAGER, // 업체 담당자
+}
