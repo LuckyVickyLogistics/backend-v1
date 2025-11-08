6bb8bca5b2a1b992383086468d80895855b1c1f6
geon <leegeonhee0204@naver.com>
2025-11-08 23:39:46 +0900

    feat(ai): AI 프롬프트 응답이 배송 담당자 근무 시간 내에서 설정되도록 구현

--- ai/ai/src/main/java/com/luckylogistics/ai/infrastructure/external/gemeni/GeminiPromptGenerator.java
+++ ai/ai/src/main/java/com/luckylogistics/ai/infrastructure/external/gemeni/GeminiPromptGenerator.java
@@ -2,6 +2,9 @@ package com.luckylogistics.ai.infrastructure.external.gemeni;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Map;
@@ -57,7 +60,9 @@ public class GeminiPromptGenerator implements AiPromptGenerator {
 				.build();
 			GenerateContentResponse response = client.models.generateContent(model, createContentList(command), config);
 
-			return AiPromptResult.from(convertResponseToInstant(response));
+			return AiPromptResult.from(
+				convertResponseToInstant(response, command.deliveryManagerStartTime(), command.deliveryManagerEndTime())
+			);
 		} catch (Exception e) {
 			throw new RuntimeException("Gemini API 호출에 실패했습니다.");
 		}
@@ -75,7 +80,8 @@ public class GeminiPromptGenerator implements AiPromptGenerator {
 			createContent("도착지 : " + command.endPoint()),
 			createContent("배송담당자근무시작시간 : " + command.deliveryManagerStartTime()),
 			createContent("배송담당자근무마감시간 : " + command.deliveryManagerEndTime()),
-			createContent("만약 요청사항에 언제까지 보내달라는 내용이 없다면 3일 후로 답변")
+			createContent("만약 요청사항에 언제까지 보내달라는 내용이 없다면 3일 후로 답변"),
+			createContent("배송 담당자 근무 시간 내에서만 최종 발송 시한을 계산하여 답변")
 		);
 	}
 
@@ -86,13 +92,31 @@ public class GeminiPromptGenerator implements AiPromptGenerator {
 			.build();
 	}
 
-	private Instant convertResponseToInstant(GenerateContentResponse response) {
+	private Instant convertResponseToInstant(GenerateContentResponse response, LocalTime startTime, LocalTime endTime) {
 		try {
 			String responseContent = mapper.readTree(response.text()).get("responseContent").asText();
-			return Instant.parse(responseContent);
+			Instant parsedResponseContent = Instant.parse(responseContent);
+			return adjustToWorkingTime(parsedResponseContent, startTime, endTime);
 		} catch (JsonProcessingException | NullPointerException | DateTimeParseException e) {
 			throw new RuntimeException("Gemini API 호출에서 예상한 값을 받을 수 없습니다.");
 		}
+	}
+
+	private Instant adjustToWorkingTime(Instant responseContent, LocalTime startTime, LocalTime endTime) {
+		ZonedDateTime zdt = responseContent.atZone(ZoneId.of("Asia/Seoul"));
+		LocalTime time = zdt.toLocalTime();
+
+		if (time.isBefore(startTime)) {
+			return replaceTime(zdt, startTime);
+		} else if (time.isAfter(endTime)) {
+			return replaceTime(zdt, endTime);
+		} else {
+			return responseContent;
+		}
+	}
+
+	private Instant replaceTime(ZonedDateTime zdt, LocalTime time) {
+		return zdt.withHour(time.getHour()).withMinute(time.getMinute()).withSecond(0).withNano(0).toInstant();
 	}
 
 }
