3e21ad5347d11f4a08a984fa694d9ff9f2d0a7ff
geon <leegeonhee0204@naver.com>
2025-11-10 00:16:59 +0900

    feat(slack): FeginClient를 통해 실제 AI 프롬프트를 받아와서 처리하도록 구현

--- slack/slack/src/main/java/com/luckylogistics/slack/infrastructure/external/client/AiServiceClientAdapter.java
+++ slack/slack/src/main/java/com/luckylogistics/slack/infrastructure/external/client/AiServiceClientAdapter.java
@@ -1,27 +1,32 @@
 package com.luckylogistics.slack.infrastructure.external.client;
 
-import java.time.Instant;
 import java.time.LocalTime;
 
 import org.springframework.stereotype.Component;
 
-import com.luckylogistics.slack.application.external.AiServiceClient;
 import com.luckylogistics.slack.application.dto.AiPromptCreatedResult;
 import com.luckylogistics.slack.application.dto.OrderCreatedResult;
+import com.luckylogistics.slack.application.external.AiServiceClient;
+import com.luckylogistics.slack.common.response.ApiResponse;
+import com.luckylogistics.slack.infrastructure.external.client.dto.AiPromptRequest;
 import com.luckylogistics.slack.infrastructure.external.client.dto.AiPromptResponse;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class AiServiceClientAdapter implements AiServiceClient {
 
 	private final AiServiceFeignClient feignClient;
 
-	// TODO: AI 서비스가 개발된 후 실제 feignClient로 요청
 	public AiPromptCreatedResult generateAiPrompt(OrderCreatedResult result, LocalTime startTime, LocalTime endTime) {
-		return AiPromptResponse.of(new AiPromptResponse(Instant.now()));
-		// return AiPromptResponse.of(feignClient.generateAiPrompt(AiPromptRequest.from(result, startTime, endTime)));
+		ApiResponse<AiPromptResponse> response = feignClient.generateAiPrompt(AiPromptRequest.from(result, startTime, endTime));
+		if (!response.success()) {
+			throw new RuntimeException(response.message());
+		}
+		return AiPromptResponse.of(response.data());
 	}
 
 }
