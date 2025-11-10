ee6b2c3b83bd9972999f30bc0ffac38d5eecfeef
jinhyeon <jingoo211@gmail.com>
2025-11-10 12:27:45 +0900

    Repository setting

--- order/order/src/main/java/com/luckylogistics/order/infrastructure/repository/OrderJPARepository.java
+++ order/order/src/main/java/com/luckylogistics/order/infrastructure/repository/OrderJPARepository.java
@@ -1,4 +1,9 @@
 package com.luckylogistics.order.infrastructure.repository;
 
-public interface OrderJPARepository {
+import com.luckylogistics.order.domain.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface OrderJPARepository extends JpaRepository<Order, UUID> {
 }
