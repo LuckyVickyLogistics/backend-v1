9eb7735a22bd64378b960874f9aaee21bd686a7c
geon <leegeonhee0204@naver.com>
2025-11-12 14:05:16 +0900

    refactor(ai): PatchMapping을 PutMapping으로 변경

--- ai/ai/src/main/java/com/luckylogistics/ai/presentation/controller/AiController.java
+++ ai/ai/src/main/java/com/luckylogistics/ai/presentation/controller/AiController.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,7 +64,7 @@ public class AiController {
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("AI 프롬프트가 조회되었습니다.", responseDto));
 	}
 
-	@PatchMapping("/{aiPromptId}/status")
+	@PutMapping("/{aiPromptId}/status")
 	public ResponseEntity<ApiResponse<Void>> updateStatus(
 		@PathVariable UUID aiPromptId, @Valid @RequestBody AiPromptStatusUpdateRequest requestDto
 	) {
