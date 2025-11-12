044f216165e0cabf0772e3eb0d7aac75c8d5ab9a
yeon-22k <pink5465y@gmail.com>
2025-11-12 22:24:54 +0900

    feat(common): common > 모듈로 교체

--- company/src/main/java/com/luckylogistics/company/application/CompanyService.java
+++ company/src/main/java/com/luckylogistics/company/application/CompanyService.java
@@ -3,8 +3,6 @@ package com.luckylogistics.company.application;
 import com.luckylogistics.company.application.dto.CompanyRequest;
 import com.luckylogistics.company.application.dto.CompanyResponse;
 import com.luckylogistics.company.application.external.HubService;
-import com.luckylogistics.company.common.exception.BusinessException;
-import com.luckylogistics.company.common.exception.ErrorCode;
 import com.luckylogistics.company.domain.CompanyDomainService;
 import com.luckylogistics.company.domain.CompanyRepository;
 import com.luckylogistics.company.domain.entity.Company;
