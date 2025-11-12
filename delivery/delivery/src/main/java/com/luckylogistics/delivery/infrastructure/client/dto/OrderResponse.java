6d1da1ea9da2f4ba86e4e060249208385c8f0211
김부경 <sa01023@naver.com>
2025-11-12 21:33:13 +0900

    feat(delivery): FeignClient용 OrderResponse DTO 및 주문 관련 Enum 추가

--- delivery/delivery/src/main/java/com/luckylogistics/delivery/infrastructure/client/dto/OrderResponse.java
+++ delivery/delivery/src/main/java/com/luckylogistics/delivery/infrastructure/client/dto/OrderResponse.java
@@ -1,36 +1,39 @@
 package com.luckylogistics.delivery.infrastructure.client.dto;
 
+import com.luckylogistics.delivery.common.enums.OrderStatus;
 import lombok.Builder;
 
 import java.util.UUID;
 
-// TODO: Order Service 연동
 @Builder
 public record OrderResponse(
-        UUID orderId,             // 주문 ID
-        UUID supplierCompanyId,   // 공급업체 ID
-        UUID customerCompanyId    // 수령업체 ID
+        UUID orderId,
+        int quantity,
+        String request,
+        OrderStatus status,
+        UUID supplierId,
+        UUID customerId,
+        UUID productId,
+        UUID deliveryId
 ) {
-    /**
-     * 응답 데이터 유효성 검증
-     */
+    // 응답 데이터 유효성 검증
     public void validate() {
         if (orderId == null) {
             throw new IllegalStateException("[OrderClient] orderId가 유효하지 않습니다");
         }
-        if (supplierCompanyId == null) {
-            throw new IllegalStateException("[OrderClient] supplierCompanyId가 유효하지 않습니다");
+        if (supplierId == null) {
+            throw new IllegalStateException("[OrderClient] supplierId가 유효하지 않습니다");
         }
-        if (customerCompanyId == null) {
-            throw new IllegalStateException("[OrderClient] customerCompanyId가 유효하지 않습니다");
+        if (customerId == null) {
+            throw new IllegalStateException("[OrderClient] customerId가 유효하지 않습니다");
         }
     }
 
     public static OrderResponse of(UUID orderId, UUID supplierCompanyId, UUID customerCompanyId) {
         return OrderResponse.builder()
                 .orderId(orderId)
-                .supplierCompanyId(supplierCompanyId)
-                .customerCompanyId(customerCompanyId)
+                .supplierId(supplierCompanyId)
+                .customerId(customerCompanyId)
                 .build();
     }
 }
