278a68d6347a4e9965f410813d407f477a6468e0
yeana07 <lee.ye.naaa@gmail.com>
2025-11-12 13:33:33 +0900

    feat(user): gateway 설정

--- user/user/src/main/java/com/luckylogistics/user/infrastructure/client/CompanyClient.java
+++ user/user/src/main/java/com/luckylogistics/user/infrastructure/client/CompanyClient.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "companies")
@@ -14,9 +15,14 @@ public interface CompanyClient {
 
 	@GetMapping("/api/v1/companies")
 	ResponseEntity<List<CompanyResponse>> getCompanies(
-		@RequestParam(required = false) String name);
+		@RequestHeader("X-Internal-Request") String internalHeader,
+		@RequestParam(required = false) String name
+	);
 
 	@GetMapping("/api/v1/{companyId}")
-	ResponseEntity<CompanyResponse> getCompany(@PathVariable(name = "companyId") UUID companyId);
+	ResponseEntity<CompanyResponse> getCompany(
+		@RequestHeader("X-Internal-Request") String internalHeader,
+		@PathVariable(name = "companyId") UUID companyId
+	);
 
 }
--- user/user/src/main/java/com/luckylogistics/user/infrastructure/client/HubClient.java
+++ user/user/src/main/java/com/luckylogistics/user/infrastructure/client/HubClient.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.luckylogistics.user.common.response.ApiResponse;
 
@@ -13,9 +14,14 @@ import com.luckylogistics.user.common.response.ApiResponse;
 public interface HubClient {
 
 	@GetMapping("/api/v1/hubs/{hubId}")
-	ApiResponse<HubResponse> getHubById(@PathVariable UUID hubId);
+	ApiResponse<HubResponse> getHubById(
+		@RequestHeader("X-Internal-Request") String internalHeader,
+		@PathVariable UUID hubId
+	);
 
 	@GetMapping("/api/v1/hubs")
-	ApiResponse<List<HubResponse>> getAllHubs();
+	ApiResponse<List<HubResponse>> getAllHubs(
+		@RequestHeader("X-Internal-Request") String internalHeader
+	);
 
 }
