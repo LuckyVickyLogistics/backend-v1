d0afd37f45ba66bfaade176f023170c69fa01817
김부경 <sa01023@naver.com>
2025-11-12 21:28:25 +0900

    feat(delivery): Eureka 클라이언트 추가 및 @EnableDiscoveryClient로 서비스 등록 활성화

--- delivery/delivery/src/main/java/com/luckylogistics/delivery/DeliveryApplication.java
+++ delivery/delivery/src/main/java/com/luckylogistics/delivery/DeliveryApplication.java
@@ -2,10 +2,12 @@ package com.luckylogistics.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class DeliveryApplication {
 
 	public static void main(String[] args) {
