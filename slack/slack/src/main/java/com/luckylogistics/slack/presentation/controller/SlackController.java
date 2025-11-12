e2d1a659bf3cde0e10cd1cff47759ca3683b2398
geon <leegeonhee0204@naver.com>
2025-11-12 13:00:55 +0900

    refactor(slack): PatchMapping을 PutMapping으로 변경

--- slack/slack/src/main/java/com/luckylogistics/slack/presentation/controller/SlackController.java
+++ slack/slack/src/main/java/com/luckylogistics/slack/presentation/controller/SlackController.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -65,7 +65,7 @@ public class SlackController {
 		return ResponseEntity.status(HttpStatus.OK).body(ApiResponse.success("Slack 워크스페이스 가입 여부가 확인되었습니다.", responseDto));
 	}
 
-	@PatchMapping("/{slackMessageId}/status")
+	@PutMapping("/{slackMessageId}/status")
 	public ResponseEntity<ApiResponse<Void>> updateStatus(
 		@PathVariable UUID slackMessageId, @Valid @RequestBody SlackStatusUpdateRequest requestDto
 	) {
