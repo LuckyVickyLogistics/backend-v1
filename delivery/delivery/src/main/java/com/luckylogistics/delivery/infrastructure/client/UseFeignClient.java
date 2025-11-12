56840b8a6a46e8d60cc61a0b9aefbbc0d7c89600
김부경 <sa01023@naver.com>
2025-11-12 21:34:53 +0900

    feat(delivery): User 서비스 연동을 위한 FeignClient 추가

--- delivery/delivery/src/main/java/com/luckylogistics/delivery/infrastructure/client/UseFeignClient.java
+++ delivery/delivery/src/main/java/com/luckylogistics/delivery/infrastructure/client/UseFeignClient.java
@@ -1,14 +1,14 @@
 package com.luckylogistics.delivery.infrastructure.client;
 
+import com.luckylogistics.delivery.common.response.ApiResponse;
 import com.luckylogistics.delivery.infrastructure.client.dto.UserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-// TODO: User Service 연동
-@FeignClient(name = "user")
+@FeignClient(name = "user", path = "/api/v1/users")
 public interface UseFeignClient {
 
-    @GetMapping("/api/v1/users/{userId}")
-    UserResponse getUser(@PathVariable("userId") Long userId);
+    @GetMapping("/{userId}")
+    ApiResponse<UserResponse> getUser(@PathVariable("userId") Long userId);
 }
