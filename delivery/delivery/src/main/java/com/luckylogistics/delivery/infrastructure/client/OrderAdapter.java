13df8ed1c23f57124e61042fe070ef55de1aaf66
김부경 <sa01023@naver.com>
2025-11-12 21:38:15 +0900

    feat(delivery): OrderFeignClient 연동을 위한 OrderAdapter 추가

--- delivery/delivery/src/main/java/com/luckylogistics/delivery/infrastructure/client/OrderAdapter.java
+++ delivery/delivery/src/main/java/com/luckylogistics/delivery/infrastructure/client/OrderAdapter.java
@@ -13,27 +13,13 @@ import java.util.UUID;
 @RequiredArgsConstructor
 class OrderAdapter implements OrderService {
 
-    // TODO: 외부 서비스 클라이언트 주입
-    // private final OrderFeignClient orderClient;
+     private final OrderFeignClient orderClient;
 
-    /**
-     * 주문 존재 여부 검증
-     */
+    // 주문 존재 여부 검증
     @Override
     public void validateOrderExists(UUID orderId) {
-        // TODO: Order Service 연동
-        // OrderResponse response = orderClient.getOrder(orderId);
-        // FIXME: 임시 하드코딩
-        OrderResponse response = OrderResponse.of(
-                orderId,
-                UUID.fromString("11111111-1111-1111-1111-111111111111"),
-                UUID.fromString("22222222-2222-2222-2222-222222222222")
-        );
+        OrderResponse response = orderClient.getOrder(orderId).data();
         response.validate();
-
-        log.debug("[Order] 주문 존재 확인 완료. orderId: {}, supplierCompanyId: {}, customerCompanyId: {}",
-                response.orderId(), response.supplierCompanyId(), response.customerCompanyId());
-
-        log.warn("[TODO] Order Service 연동 필요 - 임시 하드코딩 응답 반환");
+        log.debug("[OrderClient] 주문 존재 확인 완료. orderId: {}", response.orderId());
     }
 }
