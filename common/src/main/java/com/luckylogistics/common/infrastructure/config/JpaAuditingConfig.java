2fdf6e062ecf41053e54d0297e9345f62e875d1b
yeon-22k <pink5465y@gmail.com>
2025-11-12 23:05:17 +0900

    feat(common): common 모듈 jpaAuditing 적용

--- common/src/main/java/com/luckylogistics/common/infrastructure/config/JpaAuditingConfig.java
+++ common/src/main/java/com/luckylogistics/common/infrastructure/config/JpaAuditingConfig.java
@@ -3,6 +3,8 @@ package com.luckylogistics.common.infrastructure.config;
 import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
