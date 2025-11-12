53969d5f7deed90d21fa14ccdca262c9cfc6d2d0
geon <leegeonhee0204@naver.com>
2025-11-12 21:59:56 +0900

    refactor(order): FeignClient 호출 시 실제 response 형식에 맞춰서 변경

--- order/order/src/main/java/com/luckylogistics/order/infrastructure/client/HubFeignClient.java
+++ order/order/src/main/java/com/luckylogistics/order/infrastructure/client/HubFeignClient.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.luckylogistics.order.common.response.ApiResponse;
-import com.luckylogistics.order.infrastructure.client.dto.GetHubManagerClientResponse;
 
 @FeignClient(name = "hub", path = "/api/v1/hubs")
 public interface HubFeignClient {
 
-	@GetMapping("/hub-manager/{hubId}")
-	ApiResponse<GetHubManagerClientResponse> getHubManager(@PathVariable("hubId") UUID hubId);
+	@GetMapping("/manager/{hubId}/slack")
+	ApiResponse<String> getHubManager(@PathVariable("hubId") UUID hubId);
 
 }
