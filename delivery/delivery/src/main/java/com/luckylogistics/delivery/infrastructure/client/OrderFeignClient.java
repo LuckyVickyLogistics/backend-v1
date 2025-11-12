023b04c2cb793419c93dd3f79a1a5487fec94d77
김부경 <sa01023@naver.com>
2025-11-12 21:35:32 +0900

    feat(delivery): Order 서비스 연동을 위한 FeignClient 추가

--- delivery/delivery/src/main/java/com/luckylogistics/delivery/infrastructure/client/OrderFeignClient.java
+++ delivery/delivery/src/main/java/com/luckylogistics/delivery/infrastructure/client/OrderFeignClient.java
@@ -1,5 +1,6 @@
 package com.luckylogistics.delivery.infrastructure.client;
 
+import com.luckylogistics.delivery.common.response.ApiResponse;
 import com.luckylogistics.delivery.infrastructure.client.dto.OrderResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name = "order")
+@FeignClient(name = "order", path = "/api/v1/orders")
 public interface OrderFeignClient {
 
-    @GetMapping("/api/v1/orders/{orderId}")
-    OrderResponse getOrder(@PathVariable("orderId") UUID orderId);
+    @GetMapping("/{orderId}")
+    ApiResponse<OrderResponse> getOrder(@PathVariable("orderId") UUID orderId);
 }
