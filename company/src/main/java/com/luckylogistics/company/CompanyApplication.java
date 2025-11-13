e9cb26a8e62f7c5196a2c4fb5a15d40b236fc17c
yeon-22k <pink5465y@gmail.com>
2025-11-13 11:51:18 +0900

    refactor(company): enableJpa 삭제

--- company/src/main/java/com/luckylogistics/company/CompanyApplication.java
+++ company/src/main/java/com/luckylogistics/company/CompanyApplication.java
@@ -3,11 +3,9 @@ package com.luckylogistics.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableFeignClients
-@EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication //(scanBasePackages = "com.luckylogistics.company")
 public class CompanyApplication {
 
 	public static void main(String[] args) {
