1e814a6c1be0a6ec953c09e2339ceef68aafc544
yeon-22k <pink5465y@gmail.com>
2025-11-13 16:50:11 +0900

    refactor(company): user 관련 enum 추가

--- company/src/main/java/com/luckylogistics/company/infrastructure/client/dto/UserDetailResponse.java
+++ company/src/main/java/com/luckylogistics/company/infrastructure/client/dto/UserDetailResponse.java
@@ -1,9 +1,8 @@
 package com.luckylogistics.company.infrastructure.client.dto;
 
 import com.luckylogistics.common.enums.UserRole;
-import com.luckylogistics.user.domain.model.OrganizationType;
-import com.luckylogistics.user.domain.model.Status;
-import com.luckylogistics.user.domain.model.User;
+import com.luckylogistics.company.infrastructure.client.dto.enums.OrganizationType;
+import com.luckylogistics.company.infrastructure.client.dto.enums.Status;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -20,18 +19,4 @@ public record UserDetailResponse(
     LocalDateTime updatedAt
 ) {
 
-    public static UserDetailResponse from(User user) {
-        return new UserDetailResponse(
-            user.getUserId(),
-            user.getIdentifier(),
-            user.getUsername(),
-            user.getSlackId(),
-            user.getRole(),
-            user.getOrganizationType(),
-            user.getOrganizationId(),
-            user.getStatus(),
-            user.getCreatedAt(),
-            user.getUpdatedAt()
-        );
-    } //todo: 여기 enum이랑 mapper처리만,,
 }
